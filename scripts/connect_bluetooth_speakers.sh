fcd4e3e940074bc47ea962b7dc40122004579263
Elias Ball <contact.eliasball@gmail.com>
2022-02-08 14:24:56 +0100

    Added pulseaudio echo cancellation

--- scripts/connect_bluetooth_speakers.sh
+++ scripts/connect_bluetooth_speakers.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 printf "power on \nagent on \ndefault-agent \nconnect 7C:96:D2:3A:3D:AE \nquit" | bluetoothctl
 sleep 7s
-pactl set-default-sink bluez_sink.7C_96_D2_3A_3D_AE.a2dp_sink
+
+#pactl set-default-sink bluez_sink.7C_96_D2_3A_3D_AE.a2dp_sink
+# Activate Pulseaudio noise cancellation
+pactl unload-module module-echo-cancel
+pactl load-module module-echo-cancel
+sleep 2s
+pactl set-default-sink bluez_sink.7C_96_D2_3A_3D_AE.a2dp_sink.echo-cancel
+pactl set-default-source alsa_input.usb-SEEED_ReSpeaker_4_Mic_Array__UAC1.0_-00.analog-mono.echo-cancel
+
 systemctl --user restart raspotify.service
--- scripts/disconnect_bluetooth_speakers.sh
+++ scripts/disconnect_bluetooth_speakers.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 printf "power on \nagent on \ndefault-agent \ndisconnect 7C:96:D2:3A:3D:AE \nquit" | bluetoothctl
-pactl set-default-sink alsa_output.usb-SEEED_ReSpeaker_4_Mic_Array__UAC1.0_-00.analog-stereo
+sleep 7s
+
+#pactl set-default-sink alsa_output.usb-SEEED_ReSpeaker_4_Mic_Array__UAC1.0_-00.analog-stereo
+# Activate Pulseaudio noise cancellation
+pactl unload-module module-echo-cancel
+pactl load-module module-echo-cancel
+sleep 2s
+pactl set-default-sink alsa_output.usb-SEEED_ReSpeaker_4_Mic_Array__UAC1.0_-00.analog-stereo.echo-cancel
+pactl set-default-source alsa_input.usb-SEEED_ReSpeaker_4_Mic_Array__UAC1.0_-00.analog-mono.echo-cancel
+
 systemctl --user restart raspotify.service
